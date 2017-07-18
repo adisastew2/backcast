b6063ea3151841b6925c7822d7be9f845f73ac0a
Hack Reactor Student <student@hackreactor.com>
2017-07-17 20:06:41 -0700

    render app

--- src/views/app.js
+++ src/views/app.js
@@ -3,7 +3,9 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    console.log(window.exampleVideoData);
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
   },
 
 
